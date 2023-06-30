955ef2e0ac225dcef3660081680e488f43b79a17
Magnús Ólafsson <magnusol93@gmail.com>
2023-06-30 17:29:04 +0000

    some wip stuff

---
+++ refactor/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+// import Terminal from './components/Terminal';
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const App = () => (
+  <Container />
+);
+
+export default App;
